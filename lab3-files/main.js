d1966eb37a25e8cf4b4c6525106bb04f218b574f
gary <gc.gary94@gmail.com>
2021-05-04 20:34:13 -0700

    Fixed tweet sorting

--- lab3-files/main.js
+++ lab3-files/main.js
@@ -36,49 +36,40 @@ function handleSearch(event) {
 }
 
 
-function refreshTweets(list_dic, id_list) {
-    
-    console.log("tweet refresh")
-    
+function refreshTweets(list_dic, id_list2) {
+    //Grabbing the tweet container
     let parent = document.querySelector('.tweet-container')
+
+    //This holds all unique tweet objects
     let arrayOfTweets = []
 
-    var keys = Object.keys(list_dic);
-    var len = keys.length
-    console.log(len)
-
+    //Removes old tweets
     while( parent.firstChild ){
         parent.removeChild(parent.firstChild)
     }
 
-    //console.log(list_dic.img_url);
     console.log("populating tweets")
-    for( let i = 0; i < len; i++ ){
+    for( let i = 0; i < list_dic.length; i++ ){
         //Grab the tweet template
         let clone = document.getElementById('tweetTemplate').content.cloneNode(true)
 
         //Begins extracting neccesary information from JSON
-        clone.querySelector('.profilePic').setAttribute('src', list_dic[id_list[i]].img_url) //set profile pic
-        clone.querySelector('.author').innerHTML = list_dic[id_list[i]].user_name //set author name
-        clone.querySelector('.handle').innerHTML = "@" + list_dic[id_list[i]].screen_name //set handle
-        clone.querySelector('.tweetText').innerHTML = list_dic[id_list[i]].text //set text
-        clone.querySelector('.date').innerHTML = new Date(list_dic[id_list[i]].date).toDateString()
-        arrayOfTweets[i] = clone
-        
-        //Check for duplicate tweet ID. Then, push them into arrayOfTweeets
-    }//End for loop
-    console.log(arrayOfTweets)
+        clone.querySelector('.profilePic').setAttribute('src', list_dic[i].img_url) //set profile pic
+        clone.querySelector('.author').innerHTML = list_dic[i].user_name //set author name
+        clone.querySelector('.handle').innerHTML = "@" + list_dic[i].screen_name //set handle
+        clone.querySelector('.tweetText').innerHTML = list_dic[i].text //set text
+        clone.querySelector('.date').innerHTML = " " + new Date(list_dic[i].date).toDateString() + " " + new Date(list_dic[i].date).toLocaleTimeString()
+        arrayOfTweets.push( clone )
+    }//End for loop    
     
     arrayOfTweets.forEach(child => {
         //Then add them into HTML file
-        parent.appendChild(child)
+        parent.appendChild( child.cloneNode(true) )
     })
-
-    //middle_box.appendChild(img.src)
 }
 
 function fetch_request(){
-
+    //If the checkbox is checked, the feed will stop fetching new tweets
     let pause_checked = document.getElementById("pause").checked;
     if(!pause_checked)
     {
@@ -92,28 +83,34 @@ function fetch_request(){
             if (!id_list.includes(data.statuses[i].id))
             {
                 id_list.push(data.statuses[i].id)
-                master_list[data.statuses[i].id] =
+                master_list.push(
                 {
                     "id" : data.statuses[i].id,
                     "screen_name": data.statuses[i].user.screen_name,
                     "user_name": data.statuses[i].user.name ,
                     "img_url": data.statuses[i].user.profile_image_url_https,
-                    "date": data.statuses[i].created_at,
-                    "text": data.statuses[i].text,
-                }
+                    "date": new Date (data.statuses[i].created_at),
+                    "text": data.statuses[i].text
+                })
                 //console.log(data.statuses[i].user.screen_name)
                 //console.log(master_list[data.statuses[i].id])
             }
         }
-        master_list.sort((a, b) => (a["date"] > b["date"]) ? 1 : -1)
+        //Sorting tweet objects by time
+        master_list.sort((a, b) => {
+            return b.date.getTime() - a.date.getTime()
+        })
         let unique_id = [...new Set(id_list)]; // removes duplicates
+
+        //Appending new tweets
         refreshTweets(master_list, unique_id)
         })
         .catch(err => {
             // error catching
         console.log(err) }) 
+    }else{
+        console.log("pause checked, not getting tweets")
     }
-    console.log("pause checked, not getting tweets")
 }
 
 var intervalID = window.setInterval(fetch_request, 5000)
